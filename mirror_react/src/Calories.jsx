09c31e11837cf2ad1ff09029f412dcdca6537f5f
Stephen-Hallett <stevohallett@gmail.com>
2024-01-13 23:55:34 +1300

    added icon to calorie tracker

--- mirror_react/src/Calories.jsx
+++ mirror_react/src/Calories.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const CaloriesApp = ({calories}) => {
     return(
         <>
-        <div className="calories">{calories} Kcal</div>
+        <div className="calorieContainer">
+            <i class="fa-solid fa-utensils calorieIcon"></i>
+            <div className="calories">{calories} Kcal</div>
+        </div>
         </>
     )
 }
