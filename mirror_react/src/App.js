34758fabfdb1231c7b7b1ceacee88c1512dcf09a
Stephen-Hallett <stevohallett@gmail.com>
2025-10-31 23:34:04 +1300

    chore: Update calories API + format file

--- mirror_react/src/App.js
+++ mirror_react/src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import WeatherApp from './WeatherApp';
-import WeatherForecastApp from './WeatherForecastApp';
-import DatetimeApp from './datetime';
-import ComplimentsApp from './Compliments';
-import StravaApp from './Strava';
-import PortfolioTotalApp from './portfolioTotal';
-import React, {useEffect, useState} from "react";
-import ReactDOM from 'react-dom';
-import CaloriesApp from './Calories';
-
+import "./App.css";
+import WeatherApp from "./WeatherApp";
+import WeatherForecastApp from "./WeatherForecastApp";
+import DatetimeApp from "./datetime";
+import ComplimentsApp from "./Compliments";
+import StravaApp from "./Strava";
+import PortfolioTotalApp from "./portfolioTotal";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import CaloriesApp from "./Calories";
 
 function App() {
   const [weatherData, setWeatherdata] = useState([]);
@@ -22,118 +21,129 @@ function App() {
   const [portfolioHistory, setPortfolioHistory] = useState([]);
   const [calories, setCalories] = useState(0);
 
-  const baseURL = "http://localhost:3080"
+  const baseURL = "http://localhost:3080";
 
   const hour = new Date().getHours();
 
   useEffect(() => {
     const fetchCompliment = async () => {
-      const complimentURL = baseURL + "/compliment"
+      const complimentURL = baseURL + "/compliment";
 
       await fetch(complimentURL)
-      .then(res => res.json())
-      .then(result => {
-        setCompliment(result.compliment);
-      });
-    }
-    const interval = setInterval(() => {fetchCompliment()}, 30000);
-    return() => clearInterval(interval)
-
-  }, []);
-
-  useEffect(() => {
-      const fetchWeatherData = async () => {
-          const weatherURL = baseURL+"/weather"
-
-          await fetch(weatherURL)
-          .then(res => res.json())
-          .then(result => {
-            setWeatherdata(result);
-            //console.log(result);
-          });
-      }
-
-      const interval = setInterval(() => {fetchWeatherData()}, 60000);
-      return() => clearInterval(interval)
-  }, []);
-
-  useEffect(() => {
-    
-    const fetchForecastData = async () => { 
-      const forecastURL = baseURL+"/weatherforecast"
-
-        await fetch(forecastURL)
-        .then(res => res.json())
-        .then(result => {
-        setForecastdata(result);
-        //console.log(result);
+        .then((res) => res.json())
+        .then((result) => {
+          setCompliment(result.compliment);
         });
-    }
-    const interval = setInterval(() => {fetchForecastData()}, 90000);
-    return() => clearInterval(interval)
-}, []);
+    };
+    const interval = setInterval(() => {
+      fetchCompliment();
+    }, 30000);
+    return () => clearInterval(interval);
+  }, []);
 
-useEffect(() => {
-  const fetchDatetime = async () => {
-    const datetimeURL = baseURL + "/datetime"
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const weatherURL = baseURL + "/weather";
 
-    await fetch(datetimeURL)
-    .then(res => res.json())
-    .then(result => {
-      setDate(result.date);
-      setTime(result.time);
-    });
-  }
-  const interval = setInterval(() => {fetchDatetime()}, 1000);
-  return() => clearInterval(interval)
-}, [])
+      await fetch(weatherURL)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeatherdata(result);
+          //console.log(result);
+        });
+    };
 
-useEffect(() => {
-  const fetchStravaData = async () => { 
-    const stravaURL = baseURL+"/strava"
+    const interval = setInterval(() => {
+      fetchWeatherData();
+    }, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const fetchForecastData = async () => {
+      const forecastURL = baseURL + "/weatherforecast";
+
+      await fetch(forecastURL)
+        .then((res) => res.json())
+        .then((result) => {
+          setForecastdata(result);
+          //console.log(result);
+        });
+    };
+    const interval = setInterval(() => {
+      fetchForecastData();
+    }, 90000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const fetchDatetime = async () => {
+      const datetimeURL = baseURL + "/datetime";
+
+      await fetch(datetimeURL)
+        .then((res) => res.json())
+        .then((result) => {
+          setDate(result.date);
+          setTime(result.time);
+        });
+    };
+    const interval = setInterval(() => {
+      fetchDatetime();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const fetchStravaData = async () => {
+      const stravaURL = baseURL + "/strava";
 
       await fetch(stravaURL)
-      .then(res => res.json())
-      .then(result => {
-      setStravadata(result);
-      //console.log(result);
-      });
-  }
-  const interval = setInterval(() => {fetchStravaData()}, 60000);
-  return() => clearInterval(interval)
-}, []);
+        .then((res) => res.json())
+        .then((result) => {
+          setStravadata(result);
+          //console.log(result);
+        });
+    };
+    const interval = setInterval(() => {
+      fetchStravaData();
+    }, 60000);
+    return () => clearInterval(interval);
+  }, []);
 
-useEffect(() => {
-  const fetchPortfolioData = async () => { 
-    const portfolioURL = baseURL+"/finances"
+  useEffect(() => {
+    const fetchPortfolioData = async () => {
+      const portfolioURL = baseURL + "/finances";
 
       await fetch(portfolioURL)
-      .then(res => res.json())
-      .then(result => {
-        setPortfolioTotal(result.mostRecent);
-        setPortfolioChange(result.change);
-        setPortfolioHistory(result.portfolio);
-      //console.log(result);
-      });
-  }
-  const interval = setInterval(() => {fetchPortfolioData()}, 1000);
-  return() => clearInterval(interval)
-}, []);
+        .then((res) => res.json())
+        .then((result) => {
+          setPortfolioTotal(result.mostRecent);
+          setPortfolioChange(result.change);
+          setPortfolioHistory(result.portfolio);
+          //console.log(result);
+        });
+    };
+    const interval = setInterval(() => {
+      fetchPortfolioData();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-useEffect(() => {
-  const fetchCalories = async () => {
-    const caloriesURL = baseURL + "/calories"
+  useEffect(() => {
+    const fetchCalories = async () => {
+      const caloriesURL = "http://100.74.152.81:4000/dailyTotal";
 
-    await fetch(caloriesURL)
-    .then(res => res.json())
-    .then(result => {
-      setCalories(result.calories);
-    });
-  }
-  const interval = setInterval(() => {fetchCalories()}, 10000);
-  return() => clearInterval(interval)
-
-}, []);
+      await fetch(caloriesURL)
+        .then((res) => res.json())
+        .then((result) => {
+          setCalories(result.kcal);
+        });
+    };
+    const interval = setInterval(() => {
+      fetchCalories();
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
   //console.log(weatherData);
   //console.log(forecastData);
@@ -144,43 +154,49 @@ useEffect(() => {
     <div className="App">
       <div className="top-align">
         <div className="left-align">
-          {(!(Array.isArray(weatherData)) && !(weatherData.error)) ? (
-            <WeatherApp weatherData={weatherData} hour={hour}/>
-          ): (
+          {!Array.isArray(weatherData) && !weatherData.error ? (
+            <WeatherApp weatherData={weatherData} hour={hour} />
+          ) : (
             <div></div>
           )}
 
-          {(!(Array.isArray(weatherData)) && !(Array.isArray(forecastData)) && !(weatherData.error) && !(forecastData.error)) ? (
-            <WeatherForecastApp hour={hour} forecastData={forecastData}/>
-          ): (
+          {!Array.isArray(weatherData) &&
+          !Array.isArray(forecastData) &&
+          !weatherData.error &&
+          !forecastData.error ? (
+            <WeatherForecastApp hour={hour} forecastData={forecastData} />
+          ) : (
             <div></div>
           )}
         </div>
         <div className="center-align">
-          <PortfolioTotalApp mostRecent={portfolioTotal} change={portfolioChange}/>
+          <PortfolioTotalApp
+            mostRecent={portfolioTotal}
+            change={portfolioChange}
+          />
         </div>
         <div className="right-align">
-          <DatetimeApp date={date} time={time}/>
+          <DatetimeApp date={date} time={time} />
         </div>
       </div>
       <div className="top-middle-align"></div>
       <div className="bottom-middle-align">
-        {(compliment.length !== 0) ? (
-          <ComplimentsApp compliment={compliment}/>
+        {compliment.length !== 0 ? (
+          <ComplimentsApp compliment={compliment} />
         ) : (
           <div></div>
         )}
       </div>
       <div className="bottom-align">
         <div className="bottom-left-align">
-            {(!(Array.isArray(stravaData)) && !(stravaData.error)) ? (
-                <StravaApp stravaData={stravaData}/>
-              ): (
-                <div></div>
-              )}
+          {!Array.isArray(stravaData) && !stravaData.error ? (
+            <StravaApp stravaData={stravaData} />
+          ) : (
+            <div></div>
+          )}
         </div>
         <div className="bottom-right-align">
-          <CaloriesApp calories={calories}/>
+          <CaloriesApp calories={calories} />
         </div>
       </div>
     </div>
